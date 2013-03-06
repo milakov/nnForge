4ed2b6014c3bd1b9248d965d433e6768924473b0
milakov <maxim.milakov@gmail.com>
2013-03-06 23:30:36 +0400

    apps dir is added for user's application

---
+++ apps/App.mk
@@ -0,0 +1,23 @@
+APP_NAME=$(shell basename `pwd`)
+TARGET=../../bin/$(APP_NAME)
+CONFIG_FILE=$(TARGET).cfg
+SOURCES=$(wildcard *.cpp)
+OBJECTS=$(SOURCES:%.cpp=%.o)
+
+all: $(TARGET) $(CONFIG_FILE)
+
+$(OBJECTS): $(SOURCES)
+
+$(TARGET): $(OBJECTS) $(LDLIBSDEPEND)
+	$(CXX) -o $(TARGET) $(OBJECTS) $(LDLIBSDEPEND) $(LDFLAGS)
+
+$(CONFIG_FILE): config.cfg
+	$(RM) $(CONFIG_FILE)
+	echo 'input_data_folder=$(NNFORGE_INPUT_DATA_PATH)/$(APP_NAME)' >> $(CONFIG_FILE)
+	echo 'working_data_folder=$(NNFORGE_WORKING_DATA_PATH)/$(APP_NAME)' >> $(CONFIG_FILE)
+	cat config.cfg >> $(CONFIG_FILE)
+
+clean:
+	$(RM) $(OBJECTS) $(TARGET) $(CONFIG_FILE)
+
+.PHONY: all clean
